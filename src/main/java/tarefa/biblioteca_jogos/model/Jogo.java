2ebcf1f31d41d38c543efb48264a0c4d67ffb60a
CaioGabriel13 <caiogabrielsilvas@gmail.com>
2025-11-04 19:16:45 -0300

    Model config

--- src/main/java/tarefa/biblioteca_jogos/model/Jogo.java
+++ src/main/java/tarefa/biblioteca_jogos/model/Jogo.java
@@ -1,4 +1,47 @@
 package tarefa.biblioteca_jogos.model;
 
 public class Jogo {
+    private String titulo;
+    private String genero;
+    private String anoLancamento;
+
+    public Jogo(String titulo, String genero, String anoLancamento) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s - %s", titulo, genero, anoLancamento);
+    }
+
+    @Override
+    public int hashCode() {
+        return titulo.toLowerCase().hashCode();
+    }
 }
--- src/main/java/tarefa/biblioteca_jogos/model/Node.java
+++ src/main/java/tarefa/biblioteca_jogos/model/Node.java
@@ -1,4 +1,32 @@
 package tarefa.biblioteca_jogos.model;
 
 public class Node {
+    private Jogo jogo;
+    private Node proximo;
+
+    public Node(Jogo jogo) {
+        this.jogo = jogo;
+        this.proximo = null;
+    }
+
+    public Node(Jogo jogo, Node proximo) {
+        this.jogo = jogo;
+        this.proximo = proximo;
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public Node getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Node proximo) {
+        this.proximo = proximo;
+    }
 }
